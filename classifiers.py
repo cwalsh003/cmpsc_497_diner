1ca72cb08fe77c6969ac2e4bee6a274e91b02b7a
Sierra Schellenberg <sms97@noyce.cs.hbg.psu.edu>
2017-11-03 15:14:45 -0400

    test

---
+++ classifiers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 18:55:10 2017
+
+@author: sms97
+"""
+
+'''
+classifier ideas:
+    (1) no beef in Indian recipes
+    (2) butter in French
+    (3) dangling terms "fresh", "chopped"
+    (4) get rid of difference between plurals and singular
+
+
+
+'''
